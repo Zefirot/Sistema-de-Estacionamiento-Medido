dc87e9d654d6b0f1d82260969a6796ab996454ca
Kevin Ruiz <kevinruia77@gmail.com>
2021-06-05 16:17:02 -0300

    (fix BUG) traia mal el tipo de infraccion

--- sources/infracciones.js
+++ sources/infracciones.js
@@ -1,62 +1,65 @@
 let api = 'https://infraccionesweb.herokuapp.com/api/';
 
-function consultarInfraciones(){
+function consultarInfraciones() {
     borrar_tabla();
     let patente = document.getElementById('patente').value;
     let url = api + patente + '/infracciones';
     //deshabilitar_btn_consultar();
-    if(patente != ""){
+    if (patente != "") {
         axios.get(url).then(
-             function(response){
+            function (response) {
                 let listaInfracciones = response.data.infracciones;
-                if(listaInfracciones.length > 0){
+                if (listaInfracciones.length > 0) {
                     $("#cabecera_tabla").show();
-                    for(let i = 0; i < listaInfracciones.length; i++){
+                    for (let i = 0; i < listaInfracciones.length; i++) {
                         let infraccion = listaInfracciones[i];
                         dibujarDatos(infraccion);
                     }
-                    $("#cerrar_consulta").show();        
-                }else{
+                    $("#cerrar_consulta").show();
+                } else {
                     alert("No hay infracciones asociadas a esa patente");
                 }
             }
         );
-    }else{
+    } else {
         alert("Debe ingresar una patente");
         document.getElementById("patente").focus();
     }
     //habilitar_btn_consultar();
 }
 
-function dibujarDatos(infraccion){
-    consultar_tipo_infraccion(infraccion);
+function dibujarDatos(infraccion) {
+    let tipo_infraccion = consultar_tipo_infraccion(infraccion);
     $("#datos").prepend($(
         "<tr>" +
-            "<th scope='row'>" + infraccion.id + "</th>" +
+            "<td>" + infraccion.id + "</td>" +
             "<td>" + infraccion.montoAPagar + "</td>" +
             "<td>" + infraccion.direccionRegistrada + "</td>" +
             "<td>" + infraccion.fechaHoraRegistro + "</td>" +
-            "<td>" + localStorage.getItem("tipoInfraccion") + "</td>" +
+            "<td>" + tipo_infraccion + "</td>" +
             "<td>" +
                 "<button class='btn btn-success' style='display: none;' id='btn_consultar_ubicacion' type='submit'>Ubicación del Vehículo</button>" +
             "</td>" +
         "</tr>"));
-    if(infraccion.existeAcarreo){
+    if (infraccion.existeAcarreo) {
         $("#btn_consultar_ubicacion").show();
     }
 }
 
-function consultar_tipo_infraccion(infraccion){
-    let url_infr = api + '/tiposInfraccion/' +  infraccion.tipoInfraccion;
-    axios.get(url_infr).then(
-        function(response){
-                localStorage.setItem("tipoInfraccion", response.data.tipo.descripcion);
-            }
-        );
+function consultar_tipo_infraccion(infraccion) {
+    var url_infr = api + '/tiposInfraccion/' + infraccion.tipoInfraccion;
+    var request = new XMLHttpRequest();
+    request.open('GET', url_infr, false);  // `false` makes the request synchronous
+    request.send(null);
+    var respuesta = "";
+    if (request.status === 200) {
+        respuesta = JSON.parse(request.responseText);
+    }
+    return respuesta.tipo.descripcion;
 }
 
-function borrar_tabla(){
+function borrar_tabla() {
     $("#cabecera_tabla").hide();
-    $("#datos tr").remove(); 
-    $("#cerrar_consulta").hide(); 
+    $("#datos tr").remove();
+    $("#cerrar_consulta").hide();
 }
