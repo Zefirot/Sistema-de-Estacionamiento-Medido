87873bf1721ac698b8ee53a2b03b4c95cc95f7fe
kruiz <kevinruiza77@gmail.com>
2021-06-16 15:04:26 -0300

    Format sobre fechas

--- sources/infracciones.js
+++ sources/infracciones.js
@@ -7,7 +7,7 @@ function consultarInfraciones() {
 
     if (patente != "") {
         axios.get(url).then(
-            function (response) {
+            function(response) {
                 let listaInfracciones = response.data.infracciones;
                 if (listaInfracciones.length > 0) {
                     $("#cabecera_tabla").show();
@@ -25,21 +25,22 @@ function consultarInfraciones() {
         alert("Debe ingresar una patente");
         document.getElementById("patente").focus();
     }
-    
+
 }
 
 function dibujarDatos(infraccion) {
     let tipo_infraccion = consultar_tipo_infraccion(infraccion);
+    let fechaYhora = armarFechayHora(infraccion.fechaHoraRegistro);
     $("#datos").prepend($(
         "<tr>" +
-            "<td>" + infraccion.id + "</td>" +
-            "<td>" + infraccion.montoAPagar + "</td>" +
-            "<td>" + infraccion.direccionRegistrada + "</td>" +
-            "<td>" + infraccion.fechaHoraRegistro + "</td>" +
-            "<td>" + tipo_infraccion + "</td>" +
-            "<td>" +
-                "<a class='btn btn-success' href='mapa.html' onclick='consultarDeposito()' style='display: none;' id='btn_consultar_ubicacion'>Ubicación del Vehículo</a>" +
-            "</td>" +
+        "<td>" + infraccion.id + "</td>" +
+        "<td>" + infraccion.montoAPagar + "</td>" +
+        "<td>" + infraccion.direccionRegistrada + "</td>" +
+        "<td>" + fechaYhora + "</td>" +
+        "<td>" + tipo_infraccion + "</td>" +
+        "<td>" +
+        "<a class='btn btn-success' href='mapa.html' onclick='consultarDeposito()' style='display: none;' id='btn_consultar_ubicacion'>Ubicación del Vehículo</a>" +
+        "</td>" +
         "</tr>"));
     if (infraccion.existeAcarreo) {
         localStorage.setItem("Patente", infraccion.patente);
@@ -49,10 +50,30 @@ function dibujarDatos(infraccion) {
     }
 }
 
+function armarFechayHora(date) {
+    let fecha = new Date(date);
+    let day = fecha.getDate().toString();
+    let month = fecha.getMonth().toString();
+    let year = fecha.getFullYear();
+    let hora = (fecha.getHours() + 3).toString();
+    let minutos = fecha.getMinutes().toString();
+    let segs = fecha.getSeconds().toString();
+
+    day = day.length == 1 ? '0' + day : day;
+    month = month.length == 1 ? '0' + month : month;
+    hora = hora.length == 1 ? '0' + hora : hora;
+    minutos = minutos.length == 1 ? '0' + minutos : minutos;
+    segs = segs.length == 1 ? '0' + segs : segs;
+
+    let fecha_ret = day + "/" + month + "/" + year
+    fecha_ret += " " + hora + ":" + minutos + ":" + segs
+    return fecha_ret;
+}
+
 function consultar_tipo_infraccion(infraccion) {
     var url_infr = api + '/tiposInfraccion/' + infraccion.tipoInfraccion;
     var request = new XMLHttpRequest();
-    request.open('GET', url_infr, false); 
+    request.open('GET', url_infr, false);
     request.send(null);
     var respuesta = "";
     if (request.status === 200) {
@@ -69,5 +90,5 @@ function borrar_contenido() {
 }
 
 function consultarDeposito() {
-    localStorage.setItem("Mapa","deposito");
+    localStorage.setItem("Mapa", "deposito");
 }
